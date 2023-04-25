8dca9a57c705046858a5c9e7c13cf63f6277b0de
涎涎 <yanbo0039@yeah.net>
2023-04-25 17:12:23 +0800

    为标题赋值

--- music-client/vue.config.js
+++ music-client/vue.config.js
@@ -2,11 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: config => {
-    config.plugin('define').tap(definitions => {
-        Object.assign(definitions[0]['process.env'], {
-          NODE_HOST: '"http://localhost:8888"',
-        });
-        return definitions;
-    });
+      config.plugin('html').tap(args => {
+          args[0].title = '音乐网'
+          return args
+      })
+
+      config.plugin('define').tap(definitions => {
+          Object.assign(definitions[0]['process.env'], {
+              NODE_HOST: '"http://localhost:8888"',
+          });
+          return definitions;
+      });
   }
 })
